cb3b9f97096cc724a58bb9fcabc47a8f5cbffc93
GoldenStack <goldenfire64yt@gmail.com>
2023-05-31 18:27:11 -0500

    Bump Minestom and JUnit
    Signed-off-by: GoldenStack <goldenfire64yt@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,11 +23,11 @@ repositories {
 }
 
 dependencies {
-    compileOnly("com.github.Minestom:Minestom:51ef4779c9")
+    compileOnly("com.github.Minestom.Minestom:Minestom:9b15acf4fa")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
-    testImplementation("com.github.Minestom:Minestom:51ef4779c9")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+    testImplementation("com.github.Minestom.Minestom:Minestom:9b15acf4fa")
 }
 
 tasks.getByName<Test>("test") {
