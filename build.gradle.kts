d2febbc48e8440d8f8544229e85fa88e48c65d20
GoldenStack <git@goldenstack.net>
2025-01-25 18:20:35 -0600

    Update Minestom and publish on Maven Central

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,47 +1,72 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
-    `java-library`
-    `maven-publish`
+    id("java-library")
+    id("maven-publish")
+    id("signing")
+    id("com.vanniktech.maven.publish") version "0.30.0"
 }
 
 group = "dev.goldenstack.window"
 version = "1.0"
 description = "Useful API for Minestom inventories"
 
-java {
-    withJavadocJar()
-    withSourcesJar()
+repositories {
+    mavenCentral()
+}
 
+dependencies {
+    val minestom = "net.minestom:minestom-snapshots:96543a894f"
+    compileOnly(minestom)
+    testImplementation(minestom)
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(21))
     }
 }
 
-repositories {
-    mavenCentral()
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
 
-    maven("https://jitpack.io")
-}
+    signAllPublications()
 
-dependencies {
-    compileOnly("net.minestom:minestom-snapshots:96bf14500a")
+    coordinates("net.goldenstack", "window", version.toString())
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-    testImplementation("net.minestom:minestom-snapshots:96bf14500a")
-}
-
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
-}
-
-configure<JavaPluginExtension> {
-    withSourcesJar()
-}
-
-configure<PublishingExtension> {
-    publications {
-        create<MavenPublication>("mavenJava") {
-            from(components["java"])
+    pom {
+        name.set("window")
+        description.set("Useful API for Minestom inventories")
+        url.set("https://github.com/goldenstack/window")
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/goldenstack/window/blob/master/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("goldenstack")
+                name.set("GoldenStack")
+                email.set("git@goldenstack.net")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/goldenstack/window.git")
+            developerConnection.set("scm:git:git@github.com:goldenstack/window.git")
+            url.set("https://github.com/goldenstack/window")
         }
     }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
